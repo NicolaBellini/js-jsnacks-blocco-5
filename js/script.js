9069525fbc1dcf029c9cce1612aa78e45a8f598c
NicolaBellini <bellininicola4366@gmail.com>
2024-03-07 14:29:27 +0100

    jsnack 2

--- js/script.js
+++ js/script.js
@@ -63,3 +63,65 @@ function randomNum(min,max){
 }
 
 // JSNACK 2 //
+// JSNACK 1 //
+const cucumbers2 = [
+  {
+    varietà: "bab",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+  {
+    varietà: "ddx",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+  {
+    varietà: "aaf",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+  {
+    varietà: "sdf",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+  {
+    varietà: "wsa",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+  {
+    varietà: "yyh",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+  {
+    varietà: "bbf",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+  {
+    varietà: "wsx",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+  {
+    varietà: "bgb",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+  {
+    varietà: "scv",
+    peso: randomNum(50,300),
+    lunghezza: randomNum(10,40),
+  },
+]
+// divido le zucchine lunghe in un array e le zucchine corte in un altro array
+const long = cucumbers2.filter(cucumber => cucumber.lunghezza < 15)
+const short = cucumbers2.filter(cucumber => cucumber.lunghezza > 15)
+console.log(long.length,short.length);
+
+// sommo tutti i pesi sia delle corte che delle lunghe
+const totalWeightShort = short.reduce((total, cucumber)=> total + cucumber.peso, 0);
+const totalWeightLong = long.reduce((total, cucumber)=> total + cucumber.peso, 0)
+console.log(totalWeightShort,totalWeightLong); 

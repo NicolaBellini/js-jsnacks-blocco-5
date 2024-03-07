78d55c5f00abe3f42bcd3710458821bf02f7a0fd
NicolaBellini <bellininicola4366@gmail.com>
2024-03-07 13:58:13 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// JSNACK 1 //
+const cucumbers = [
+  {
+    varietà: "bab",
+    peso: 120,
+    lunghezza: 20,
+  },
+  {
+    varietà: "ddx",
+    peso: 120,
+    lunghezza: 20,
+  },
+  {
+    varietà: "aaf",
+    peso: 120,
+    lunghezza: 20,
+  },
+  {
+    varietà: "sdf",
+    peso: 120,
+    lunghezza: 20,
+  },
+  {
+    varietà: "wsa",
+    peso: 120,
+    lunghezza: 20,
+  },
+  {
+    varietà: "yyh",
+    peso: 120,
+    lunghezza: 20,
+  },
+  {
+    varietà: "bbf",
+    peso: 120,
+    lunghezza: 20,
+  },
+  {
+    varietà: "wsx",
+    peso: 120,
+    lunghezza: 20,
+  },
+  {
+    varietà: "bgb",
+    peso: 120,
+    lunghezza: 20,
+  },
+  {
+    varietà: "scv",
+    peso: 120,
+    lunghezza: 20,
+  },
+]
+
+console.log(cucumbers.length);
+
+function randomNum
